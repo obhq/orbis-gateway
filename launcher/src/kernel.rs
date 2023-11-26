3ad4d861f910a1564358a236652be4effe978bca
Putta Khunchalee <ultimaweapon@outlook.com>
2023-11-26 12:23:04 +0700

    Adds errno

--- launcher/src/kernel.rs
+++ launcher/src/kernel.rs
@@ -1,6 +1,13 @@
 use crate::syscalls::{sys_dynlib_dlsym, sys_dynlib_load_prx};
-use core::ffi::CStr;
+use core::ffi::{c_int, CStr};
+use core::mem::transmute;
 use core::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+
+pub fn errno() -> c_int {
+    let f: Option<unsafe extern "C" fn() -> *mut c_int> =
+        unsafe { transmute(ERRNO.load(Ordering::Relaxed)) };
+    unsafe { *f.unwrap()() }
+}
 
 /// # Panics
 /// If called a second time.
@@ -32,8 +39,15 @@ pub fn init_kernel() {
 }
 
 static LOADED: AtomicBool = AtomicBool::new(false);
+static ERRNO: AtomicUsize = AtomicUsize::new(0);
 static LOAD_START_MODULE: AtomicUsize = AtomicUsize::new(0);
-static FUNCTION_TABLE: [(&'static CStr, &'static AtomicUsize); 1] = [(
-    unsafe { CStr::from_bytes_with_nul_unchecked(b"sceKernelLoadStartModule\0") },
-    &LOAD_START_MODULE,
-)];
+static FUNCTION_TABLE: [(&'static CStr, &'static AtomicUsize); 2] = [
+    (
+        unsafe { CStr::from_bytes_with_nul_unchecked(b"__error\0") },
+        &ERRNO,
+    ),
+    (
+        unsafe { CStr::from_bytes_with_nul_unchecked(b"sceKernelLoadStartModule\0") },
+        &LOAD_START_MODULE,
+    ),
+];
